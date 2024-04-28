4045cf61a473c119929950b403069dcb4e3683e3
gpcsantos <gpcsantos@gmail.com>
2024-04-28 17:42:43 -0300

    Projeto do zero (ciclo de vido useState, useEfect)

--- Modulo02/projeto_do_zero/src/App.jsx
+++ Modulo02/projeto_do_zero/src/App.jsx
@@ -1,7 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+// COMPONENTES DE CLASSE
+// class App extends Component {
+//   state = {
+//     usarios: ['Palbo', 'José', 'Manoel'],
+//   };
+//   render() {
+//     const { usuarios } = this.state;
+//     return (
+//       <div className='App'>
+//         <h1>Hello</h1>
+//         {usuarios.map(item => (
+//           <p>{item}</p>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// COMPONENTE FUNCIONAL
 const App = () => {
+  const [usuario, setUsuario] = useState('');
+  const [usuarios, setUsuarios] = useState(['Palbo', 'José', 'Manoel']);
+  const [count, setCount] = useState(0);
+
+  const handleAddUser = () => {
+    setUsuarios([...usuarios, usuario]);
+    setUsuario('');
+  };
+  useEffect(() => {
+    setCount(usuarios.length);
+  }, [usuarios]);
+
   return (
     <div className='App'>
-      <h1> Hello!!!</h1>
+      <h1>Hello</h1>
+      <div>
+        <p>Count: {count}</p>
+        <input
+          type='text'
+          value={usuario}
+          onChange={event => setUsuario(event.target.value)}
+        />
+        <button onClick={handleAddUser}>Adiciona</button>
+      </div>
+      <hr />
+      {usuarios.map(item => (
+        <p>{item}</p>
+      ))}
     </div>
   );
 };
