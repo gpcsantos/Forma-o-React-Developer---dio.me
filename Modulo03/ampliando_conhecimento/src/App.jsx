20aa972ec8f541b2c8dd95e2f04c58f3c8021d3a
gpcsantos <gpcsantos@gmail.com>
2024-04-30 15:19:45 -0300

    Rotas - Ampliando conhecimentos

---
+++ Modulo03/ampliando_conhecimento/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/login';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Modulo03/ampliando_conhecimento/src/pages/login/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+  return (
+    <>
+      <div>LOGIN</div>
+      <Link to='/'>Volta para Home</Link>
+    </>
+  );
+}
